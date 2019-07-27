ee2e21250e3186e2fd45588e872b0b4627baf6bc
mzy <1092341705@qq.com>
2019-07-27 20:35:34 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vant from 'vant'
+import '../node_modules/vant/lib/index.css'
 Vue.config.productionTip = false
+Vue.use(Vant)
 
 new Vue({
   router,
   store,
+  Vant,
   render: h => h(App)
 }).$mount('#app')
